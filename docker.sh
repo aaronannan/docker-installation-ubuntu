6ca2ddc6e7e8d31378074b125fbee17be4e32d7b
aaronannan <42162864+aaronannan@users.noreply.github.com>
2019-02-06 10:59:40 -0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,7 +4,7 @@ sudo apt-get remove docker docker-engine docker.io
 
 sudo apt-get update
 
-sudo apt-get install \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
